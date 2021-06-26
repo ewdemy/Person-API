0056a7afa5fef04ff5554b6644ccae4f88c18119
ewdemy <ewdemy@hotmail.com>
2021-06-26 18:44:41 -0300

    implementado repository

---
+++ src/main/java/com/mrcruz/personapi/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.mrcruz.personapi.repository;
+
+import com.mrcruz.personapi.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
